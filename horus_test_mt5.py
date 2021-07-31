e697c6e9e7ed989a38ba11754b827d1b71ccf40c
Meseverri <meseverrimarquez5@gmail.com>
2021-07-31 21:44:15 +0200

    commit 31/7/2021

--- horus_test_mt5.py
+++ horus_test_mt5.py
@@ -24,8 +24,8 @@ euraud_ticks = mt5.copy_ticks_from("EURAUD", datetime(2020,1,28,13), 1000, mt5.C
 audusd_ticks = mt5.copy_ticks_range("AUDUSD", datetime(2020,1,27,13), 1000, mt5.COPY_TICKS_ALL)
  
 # obtenemos con distintos métodos las barras de diferentes instrumentos
-eurusd_rates = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_M1, datetime(2020,1,28,13), 1000)
-eurgbp_rates = mt5.copy_rates_from_pos("EURGBP", mt5.TIMEFRAME_M1, 0, 1000)
+eurusd_rates = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_M1, datetime.today(), 1000)
+eurgbp_rates = mt5.copy_rates_from("EURGBP", mt5.TIMEFRAME_M1, datetime.today(), 1000)
 eurcad_rates = mt5.copy_rates_range("EURCAD", mt5.TIMEFRAME_M1, datetime(2020,1,27,13), datetime(2020,1,28,13))
 print("LAST ERROR----->",mt5.last_error())
 # finalizamos la conexión con MetaTrader 5
@@ -50,6 +50,8 @@ for val in eurcad_rates[:10]: print(val)
 #PLOT
 # creamos un DataFrame de los datos obtenidos
 ticks_frame = pd.DataFrame(euraud_ticks)
+ticks_frame['time']=pd.to_datetime(ticks_frame['time'], unit='s')
+print(ticks_frame.head(5))
 # dibujamos los ticks en el gráfico
 plt.plot(ticks_frame['time'], ticks_frame['ask'], 'r-', label='ask')
 plt.plot(ticks_frame['time'], ticks_frame['bid'], 'b-', label='bid')
