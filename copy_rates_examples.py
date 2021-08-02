96d9f7a1864d0ceab916c22ecd93cbf9e649266c
Meseverri <meseverrimarquez5@gmail.com>
2021-08-02 20:47:17 +0200

    commit 2-8-2021

--- copy_rates_examples.py
+++ copy_rates_examples.py
@@ -33,23 +33,63 @@ rates = mt5.copy_rates_from("EURUSD", mt5.TIMEFRAME_H1, utc_from, 1000)
 # finalizamos la conexión con el terminal MetaTrader 5
 mt5.shutdown()
 # mostramos cada elemento de los datos obtenidos en una nueva línea
+
+
+
+
+
+
 print("Mostramos los datos obtenidos como son")
-for rate in rates:
+for rate in rates[-10:]:
     print(rate)
  
 # creamos un DataFrame de los datos obtenidos
 rates_frame = pd.DataFrame(rates)
 # convertimos la hora en segundos al formato datetime
-rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
-rates_frame['variacion log open']=np.log(rates_frame['open'].shift(1) /rates_frame['open'])
 
+rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
+rates_frame.set_index("time")
+rates_frame['variacion log open']=np.log(rates_frame['open'].shift(1) /rates_frame['open'])
+pos_log=rates_frame[rates_frame['variacion log open']>=0]
+neg_log=rates_frame[rates_frame['variacion log open']<=0]
 # mostramos los datos
 print("\nMostramos el frame de datos con la información")
-print(rates_frame)  
-print(rates_frame['variacion log open'].std())
-plt.plot(rates_frame["open"],rates_frame["variacion log open"], 'r.', label="variacion log open")
 
- 
+print(rates_frame)  
+print("\n Crecimientos \n",pos_log)
+print("\n Decrecimientos \n",neg_log)
+print(rates_frame['variacion log open'].std())
+
+fig,axs=plt.subplots(2,3)
+axs[0,0].plot(rates_frame["open"],rates_frame["variacion log open"],"r.", label="variacion log open")
+axs[0,0].legend(loc='upper left')
+axs[0,0].set_title('EURUSD')
+
+axs[0,1].plot(rates_frame["time"],rates_frame["open"],"r--", label="Open")
+axs[0,1].legend(loc='upper left')
+axs[0,1].set_title('EURUSD')
+
+axs[1,0].plot(pos_log["open"],pos_log["variacion log open"],"b.",label="variacion log positiva open")
+axs[1,0].legend(loc='upper left')
+axs[1,0].set_title('EURUSD')
+
+axs[1,1].plot(neg_log["open"],neg_log["variacion log open"],"b.",label="variacion log negativa open")
+axs[1,1].legend(loc='upper left')
+axs[1,1].set_title('EURUSD')
+
+axs[1,2].plot(rates_frame["tick_volume"],rates_frame["variacion log open"],"g.",label="tick volume")
+#plt.subplot(311)
+#plt.plot(rates_frame["open"],rates_frame["variacion log open"],"r.", label="variacion log open")
+#plt.title('EURUSD')
+#plt.subplot(312)
+#plt.plot(rates_frame["time"],rates_frame["open"],"r--", label="Open")
+#plt.subplot(313)
+#plt.plot(pos_log["open"],pos_log["variacion log open"],"b.",label="variacion log positiva open") 
+#plt.subplot(221)
+#plt.plot(neg_log["open"],neg_log["variacion log open"],"b.",label="variacion log negativa open") 
+#plt.subplot(222)
+#plt.plot(rates_frame["tick_volume"],rates_frame["variacion log open"],"g.",label="tick volume")
+
 # mostramos los rótulos
 plt.legend(loc='upper left')
  
@@ -58,3 +98,6 @@ plt.title('EURUSD')
  
 # mostramos el gráfico
 plt.show()
+
+
+ 
